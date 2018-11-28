bcbe738bac634d393decc9343ceb0aff011b488b
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-11-27 22:48:34 -0600

    Create denos_get_started.sh

---
+++ denos_get_started.sh
@@ -0,0 +1,20 @@
+# DenOS - Debian Linux build from scratch with Debootstrap#
+# Bernardino Lopez [ bernardino.lopez@gmail.com ]
+# Brains R Like Books only work when they are open - PHPOpenMonitor -
+# 11.27.18
+
+# denos_get_started.sh - Initial Pre-Req Download Script.
+# Execute the following command in a Terminal to get started the DenOS download scripts...
+# wget https://raw.githubusercontent.com/dinooz/denos/master/Denos_HowTo.txt
+
+wget https://raw.githubusercontent.com/dinooz/denos/master/Denos_HowTo.txt
+wget https://raw.githubusercontent.com/dinooz/denos/master/Denos_HowTo.txt
+wget https://raw.githubusercontent.com/dinooz/denos/master/Denos_HowTo.txt
+wget https://raw.githubusercontent.com/dinooz/denos/master/Denos_HowTo.txt
+wget https://raw.githubusercontent.com/dinooz/denos/master/Denos_HowTo.txt
+wget https://raw.githubusercontent.com/dinooz/denos/master/Denos_HowTo.txt
+wget https://raw.githubusercontent.com/dinooz/denos/master/Denos_HowTo.txt
+wget https://raw.githubusercontent.com/dinooz/denos/master/Denos_HowTo.txt
+wget https://raw.githubusercontent.com/dinooz/denos/master/Denos_HowTo.txt
+
+chmod 755 *.sh
