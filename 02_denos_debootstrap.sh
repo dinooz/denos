1f0543e0b98a40787964209e752cfc7e21904f5d
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-11-27 22:59:21 -0600

    Create 02_denos_debootstrap.sh

---
+++ 02_denos_debootstrap.sh
@@ -0,0 +1,22 @@
+# DenOS - Debian Linux build from scratch with Debootstrap#
+# Bernardino Lopez [ bernardino.lopez@gmail.com ]
+# 11.27.18
+
+# 02_denos_debootstrap.sh - Debootstrap our Distro
+# As user Execute the script in a Terminal 
+
+# export LIVE_BOOT=LIVE_BOOT64
+require denos_config.txt
+
+mkdir $HOME/$LIVE_BOOT
+
+sudo debootstrap \
+--arch=amd64 \
+--variant=minbase \
+stretch \
+$HOME/$LIVE_BOOT/chroot \
+http://ftp.us.debian.org/debian
+
+cp ./03_Dinux_Chroot.sh $HOME/$LIVE_BOOT/chroot
+
+sudo chroot $HOME/$LIVE_BOOT/chroot
