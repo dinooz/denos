d6f66a624af6335fb9095fb4469c3623d1a5907c
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-12-04 20:53:22 -0600

    Create 03_denos_chroot_e17.sh

---
+++ 03_denos_chroot_e17.sh
@@ -0,0 +1,33 @@
+# DenOS - Debian Linux build from scratch with Debootstrap#
+# Bernardino Lopez [ bernardino.lopez@gmail.com ]
+# 11.27.18
+
+# 03_denos_chroot.sh - Customize your Distro
+# As root in chroot. Execute the script in a Terminal 
+# ./03_denos_chroot_lxde.sh
+
+# export LIVE_BOOT=LIVE_BOOT64
+source ./denos_config.txt
+
+#echo "debian-live" > /etc/hostname
+echo $DISTRO_NAME > /etc/hostname 
+
+apt-cache search linux-image
+
+apt-get update && \
+apt-get install -y --no-install-recommends \
+    linux-image-4.9.0-8-amd64 \
+    live-boot \
+    systemd-sysv
+
+apt-get install -y --no-install-recommends \
+    network-manager net-tools wireless-tools wpagui \
+    curl openssh-server openssh-client \
+    blackbox xserver-xorg-core xserver-xorg xinit xterm \
+    screenfetch screen lxterminal vim \
+    nano e17* && \
+apt-get clean
+
+passwd root
+
+exit
