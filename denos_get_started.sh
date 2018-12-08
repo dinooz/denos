83b2b2ec344043268e27594dadd3a797344bdae9
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-12-07 20:19:48 -0600

    Update denos_get_started.sh

--- denos_get_started.sh
+++ denos_get_started.sh
@@ -11,6 +11,7 @@
 wget https://github.com/dinooz/denos/raw/master/Denos_HowTo.txt
 wget https://github.com/dinooz/denos/raw/master/01_denos_pre_reqs.sh
 wget https://github.com/dinooz/denos/raw/master/02_denos_debootstrap.sh
+
 wget https://github.com/dinooz/denos/raw/master/03_denos_chroot.sh
 wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_lxde.sh
 wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_xfce.sh
@@ -18,6 +19,14 @@ wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_kde.sh
 wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_mate.sh
 wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_plasma.sh
 wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_e17.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_i3.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_openbox.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_awesome.sh
+# Working on this scripts...
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_cinnamon.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_pantheon.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_gnome.sh
+
 wget https://github.com/dinooz/denos/raw/master/04_denos_MakeSquashfs.sh
 wget https://github.com/dinooz/denos/raw/master/05_denos_BiosUEFI_ISO.sh
 wget https://github.com/dinooz/denos/raw/master/denos_config.txt
