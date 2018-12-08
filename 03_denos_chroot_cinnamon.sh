d236cd461bd3b3c2261b77775399d6097f486ba2
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-12-07 20:16:10 -0600

    Add files via upload

---
+++ 03_denos_chroot_cinnamon.sh
@@ -0,0 +1,42 @@
+# DenOS - Debian Linux build from scratch with Debootstrap#
+# Bernardino Lopez [ bernardino.lopez@gmail.com ]
+# 11.27.18
+
+# 03_denos_chroot.sh - Customize your Distro
+# As root in chroot. Execute the script in a Terminal 
+# ./03_denos_chroot_lxde.sh
+
+# export LIVE_BOOT=LIVE_BOOT64
+source ./denos_config.txt
+
+#echo "debian-live" > /etc/hostname
+echo $DISTRO_NAME > /etc/hostname 
+
+apt-cache search linux-image
+
+apt-get update && \
+apt-get install -y --no-install-recommends \
+    linux-image-4.9.0-8-amd64 \
+    live-boot \
+    systemd-sysv
+
+apt-get install -y --no-install-recommends \
+    network-manager net-tools wireless-tools wpagui \
+    curl openssh-server openssh-client \
+    xserver-xorg-core xserver-xorg xinit xterm \
+    screenfetch screen lxterminal vim \
+    cinnamon-desktop-environment \
+    nano && \
+apt-get clean
+
+#echo "exec cinnamon-session" > /root/.xinitrc
+#chmod 755 /root/.xinitrc
+
+#echo "user-session=cinnamon" >> /etc/lightdm/lightdm.conf
+
+echo -e "127.0.0.1\tlocalhost" > /etc/hosts
+echo -e "127.0.0.1\tDenOS" >> /etc/hosts
+
+passwd root
+
+exit
