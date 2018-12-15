25aec53985a38e2b26ce06a6d4eec36112c9c8f6
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-12-15 12:16:13 -0600

    Create denos_hd_get_started.sh

---
+++ denos_hd_install/denos_hd_get_started.sh
@@ -0,0 +1,14 @@
+# DenOS - Debian Linux build from scratch with Debootstrap#
+# Bernardino Lopez [ bernardino.lopez@gmail.com ]
+# 11.27.18
+
+# denos_get_started.sh - Initial Pre-Req Download Script.
+# Execute the following command in a Terminal to get started the DenOS download scripts...
+# wget https://raw.githubusercontent.com/dinooz/denos/master/denos_get_started.sh
+# chmod 755 denos_get_started.sh
+# ./denos_hd_get_started.sh
+
+wget https://github.com/dinooz/denos/raw/master/Denos_HowTo.txt
+wget https://github.com/dinooz/denos/raw/master/01_denos_pre_reqs.sh
+
+chmod 755 *.sh
