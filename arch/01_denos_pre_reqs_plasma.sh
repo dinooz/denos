b19dae1d25a010ce4d441d73d3aef7aca8d70ea6
Bernardino Lopez <bernardino_lopez@yahoo.com>
2020-05-16 21:31:16 -0500

    Update 01_denos_pre_reqs_plasma.sh

--- arch/01_denos_pre_reqs_plasma.sh
+++ arch/01_denos_pre_reqs_plasma.sh
@@ -2,12 +2,14 @@
 # Bernardino Lopez [ bernardino.lopez@gmail.com ]
 # 05.15.20
 
-# 01_denos_pre_reqs_xfce.sh - Arch Pre-Req Installation
+# 01_denos_pre_reqs_plasma.sh - Arch Pre-Req Installation
 # As root Execute the following command in a Terminal to get started the 
 
-pacstrap /mnt base base-devel linux linux-firmware grub grub-bios os-prober net-tools networkmanager mtr dhcp mkinitcpio dracut openssh vim wget screen htop xorg xorg-xinit xorg-twm xorg-xclock xterm xfce4 xfce4-goodies
+pacstrap /mnt base base-devel linux linux-firmware grub grub-bios os-prober net-tools networkmanager mtr dhcp mkinitcpio dracut openssh vim wget screen htop xorg xorg-xinit xorg-twm xorg-xclock xterm plasma sddm
 
-echo " "
+
+
+echo " NOTE Don't forget to Enable SDDM prior to exit Chroot -> systemctl enable sddm "
 echo "Edit  ./denos_config.txt to customize your Distro Settings"
 echo "Execute as root ./02_denos_genfstab.sh"
 echo " "
